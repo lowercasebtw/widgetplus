006e1bd9c7c1a4057973e2e6021c343e1aaab491
lowercasebtw <jejojora2@gmail.com>
2026-04-25 20:55:31 -0500

    Make fields in GuiPipelineOverrides optional

--- src/main/java/btw/lowercase/widgetplus/impl/GuiPipelineOverrides.java
+++ src/main/java/btw/lowercase/widgetplus/impl/GuiPipelineOverrides.java
@@ -5,37 +5,30 @@ import com.mojang.blaze3d.pipeline.ColorTargetState;
 import com.mojang.blaze3d.pipeline.RenderPipeline;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.resources.Identifier;
+
+import java.util.Optional;
 
 public class GuiPipelineOverrides {
     public static final Codec<GuiPipelineOverrides> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-            Identifier.CODEC.orElse(RenderPipelines.GUI.getFragmentShader()).fieldOf("vertex").forGetter(overrides -> overrides.vertexShader),
-            Identifier.CODEC.orElse(RenderPipelines.GUI.getVertexShader()).fieldOf("fragment").forGetter(overrides -> overrides.fragmentShader),
-            Utils.COLOR_TARGET_STATE_CODEC.orElse(RenderPipelines.GUI.getColorTargetState()).fieldOf("color_target").forGetter(overrides -> overrides.colorTargetState)
+            Identifier.CODEC.optionalFieldOf("vertex").forGetter(overrides -> overrides.vertexShader),
+            Identifier.CODEC.optionalFieldOf("fragment").forGetter(overrides -> overrides.fragmentShader),
+            Utils.COLOR_TARGET_STATE_CODEC.optionalFieldOf("color_target").forGetter(overrides -> overrides.colorTargetState)
     ).apply(instance, GuiPipelineOverrides::new));
 
-    private final Identifier vertexShader;
-    private final Identifier fragmentShader;
-    private final ColorTargetState colorTargetState;
+    private final Optional<Identifier> vertexShader;
+    private final Optional<Identifier> fragmentShader;
+    private final Optional<ColorTargetState> colorTargetState;
 
-    GuiPipelineOverrides(final Identifier vertexShader, final Identifier fragmentShader, final ColorTargetState colorTargetState) {
+    GuiPipelineOverrides(final Optional<Identifier> vertexShader, final Optional<Identifier> fragmentShader, final Optional<ColorTargetState> colorTargetState) {
         this.vertexShader = vertexShader;
         this.fragmentShader = fragmentShader;
         this.colorTargetState = colorTargetState;
     }
 
     public void apply(final RenderPipeline.Builder builder) {
-        if (this.vertexShader != null) {
-            builder.withVertexShader(this.vertexShader);
-        }
-
-        if (this.fragmentShader != null) {
-            builder.withFragmentShader(this.fragmentShader);
-        }
-
-        if (this.colorTargetState != null) {
-            builder.withColorTargetState(this.colorTargetState);
-        }
+        this.vertexShader.ifPresent(builder::withVertexShader);
+        this.fragmentShader.ifPresent(builder::withFragmentShader);
+        this.colorTargetState.ifPresent(builder::withColorTargetState);
     }
 }
