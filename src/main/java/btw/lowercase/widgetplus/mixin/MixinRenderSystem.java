6be9d487e0300883c122e6edb263b052df6e6436
lowercasebtw <jejojora2@gmail.com>
2026-04-26 20:52:20 -0500

    Enabled checks

--- src/main/java/btw/lowercase/widgetplus/mixin/MixinRenderSystem.java
+++ src/main/java/btw/lowercase/widgetplus/mixin/MixinRenderSystem.java
@@ -1,6 +1,7 @@
 package btw.lowercase.widgetplus.mixin;
 
 import btw.lowercase.widgetplus.WidgetPlus;
+import btw.lowercase.widgetplus.config.WidgetPlusConfig;
 import btw.lowercase.widgetplus.impl.management.WidgetPlusDynamicUniforms;
 import com.mojang.blaze3d.TracyFrameCapture;
 import com.mojang.blaze3d.buffers.GpuBufferSlice;
@@ -8,6 +9,7 @@ import com.mojang.blaze3d.systems.GpuDevice;
 import com.mojang.blaze3d.systems.RenderPass;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.MouseHandler;
 import net.minecraft.client.renderer.DynamicUniforms;
 import org.joml.Vector2d;
 import org.spongepowered.asm.mixin.Mixin;
@@ -30,7 +32,7 @@ public abstract class MixinRenderSystem {
     @Inject(method = "flipFrame", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/DynamicUniforms;reset()V", shift = At.Shift.AFTER))
     private static void widgetplus$resetUniforms(final TracyFrameCapture tracyFrameCapture, final CallbackInfo ci) {
         final WidgetPlusDynamicUniforms dynamicUniforms = WidgetPlus.dynamicUniforms();
-        if (dynamicUniforms != null) {
+        if (WidgetPlusConfig.instance().enabled && dynamicUniforms != null) {
             dynamicUniforms.reset();
         }
     }
@@ -38,9 +40,10 @@ public abstract class MixinRenderSystem {
     @Inject(method = "getDynamicUniforms", at = @At("HEAD"))
     private static void widgetplus$setupUbo(final CallbackInfoReturnable<DynamicUniforms> cir) {
         final WidgetPlusDynamicUniforms dynamicUniforms = WidgetPlus.dynamicUniforms();
-        if (dynamicUniforms != null) {
+        if (WidgetPlusConfig.instance().enabled && dynamicUniforms != null) {
+            final MouseHandler mouseHandler = Minecraft.getInstance().mouseHandler;
             widgetplus$dynamicUniforms = dynamicUniforms.write(
-                    new Vector2d(Minecraft.getInstance().mouseHandler.xpos(), Minecraft.getInstance().mouseHandler.ypos()),
+                    new Vector2d(mouseHandler.xpos(), mouseHandler.ypos()),
                     0, 0 // TODO
             );
         }
@@ -48,7 +51,7 @@ public abstract class MixinRenderSystem {
 
     @Inject(method = "bindDefaultUniforms", at = @At("TAIL"))
     private static void widgetplus$bindCustomUniform(final RenderPass renderPass, final CallbackInfo ci) {
-        if (widgetplus$dynamicUniforms != null) {
+        if (WidgetPlusConfig.instance().enabled && widgetplus$dynamicUniforms != null) {
             renderPass.setUniform("WPData", widgetplus$dynamicUniforms);
         }
     }
