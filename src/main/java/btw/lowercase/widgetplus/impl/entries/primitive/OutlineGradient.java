e44fb00a8cd99899d33e0dd49664c114ea7aeda1
lowercasebtw <jejojora2@gmail.com>
2026-04-27 17:24:25 -0500

    Add "width" to line primitives
    (cherry picked from commit 07362fd4065e5d82533fcde09887be298897f00c)

--- src/main/java/btw/lowercase/widgetplus/impl/entries/primitive/OutlineGradient.java
+++ src/main/java/btw/lowercase/widgetplus/impl/entries/primitive/OutlineGradient.java
@@ -1,17 +1,19 @@
 package btw.lowercase.widgetplus.impl.entries.primitive;
 
 import btw.lowercase.widgetplus.impl.util.GuiPipelineOverrides;
+import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.util.ExtraCodecs;
 
 import java.util.Optional;
 
-public record OutlineGradient(int startColor, int endColor,
+public record OutlineGradient(int startColor, int endColor, float lineWidth,
                               Optional<GuiPipelineOverrides> pipelineOverrides) implements PrimitiveFunction {
     public static final MapCodec<OutlineGradient> MAP_CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
             ExtraCodecs.STRING_ARGB_COLOR.fieldOf("start_color").forGetter(OutlineGradient::startColor),
             ExtraCodecs.STRING_ARGB_COLOR.fieldOf("end_color").forGetter(OutlineGradient::endColor),
+            Codec.FLOAT.optionalFieldOf("width", 1.0F).forGetter(OutlineGradient::lineWidth),
             GuiPipelineOverrides.CODEC.optionalFieldOf("pipeline_overrides").forGetter(OutlineGradient::pipelineOverrides)
     ).apply(instance, OutlineGradient::new));
 
