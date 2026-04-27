6b7ffadf783496f04d6cbc29c5a9ec7ca1fb9e0f
lowercasebtw <jejojora2@gmail.com>
2026-04-27 17:14:54 -0500

    Update Bounds

--- src/main/java/btw/lowercase/widgetplus/impl/util/Bounds.java
+++ src/main/java/btw/lowercase/widgetplus/impl/util/Bounds.java
@@ -5,10 +5,10 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import java.util.Optional;
 
-public record Bounds(boolean relative, Optional<Integer> x, Optional<Integer> y, Optional<Integer> width,
+public record Bounds(boolean absolute, Optional<Integer> x, Optional<Integer> y, Optional<Integer> width,
                      Optional<Integer> height) {
     public static Codec<Bounds> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-            Codec.BOOL.optionalFieldOf("relative", false).forGetter(Bounds::relative),
+            Codec.BOOL.optionalFieldOf("absolute", false).forGetter(Bounds::absolute),
             Codec.INT.optionalFieldOf("x").forGetter(Bounds::x),
             Codec.INT.optionalFieldOf("y").forGetter(Bounds::y),
             Codec.INT.optionalFieldOf("width").forGetter(Bounds::width),
@@ -16,10 +16,12 @@ public record Bounds(boolean relative, Optional<Integer> x, Optional<Integer> y,
     ).apply(instance, Bounds::new));
 
     public int getX(final int x) {
-        return this.relative ? x + this.x.orElse(0) : x;
+        final int ox = this.x.orElse(0);
+        return this.absolute ? ox : x + ox;
     }
 
     public int getY(final int y) {
-        return this.relative ? y + this.y.orElse(0) : y;
+        final int oy = this.y.orElse(0);
+        return this.absolute ? oy : y + oy;
     }
 }
