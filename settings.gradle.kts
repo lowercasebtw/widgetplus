ed61799f1ee94d287434b520ae39151df64c6a0b
lowercasebtw <jejojora2@gmail.com>
2026-04-28 18:16:00 -0500

    Stonecutter

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,37 @@
 pluginManagement {
-	repositories {
-		maven {
-			name = "Fabric"
-			url = uri("https://maven.fabricmc.net/")
-		}
-		mavenCentral()
-		gradlePluginPortal()
-	}
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.fabricmc.net")
+        maven("https://maven.kikugie.dev/snapshots")
+        maven("https://repo.polyfrost.cc/releases")
+    }
+}
 
-	plugins {
-		id("net.fabricmc.fabric-loom") version providers.gradleProperty("loom_version")
-	}
+plugins {
+    id("dev.kikugie.stonecutter") version "0.9.1"
+}
+
+stonecutter {
+    kotlinController = true
+    centralScript = "build.gradle.kts"
+    create(rootProject) {
+        fun mc(mcVersion: String, loaders: Iterable<String>) {
+            for (loader in loaders) {
+                version("$mcVersion-$loader", mcVersion)
+            }
+        }
+
+        mc("26.1", listOf("fabric"))
+
+        vcsVersion = "26.1-fabric"
+    }
+}
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs")
+    }
 }
 
 rootProject.name = "WidgetPlus"
---
+++ stonecutter.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("dev.kikugie.stonecutter")
+}
+
+stonecutter active "26.1-fabric" /* [SC] DO NOT EDIT */
+
+stonecutter {
+}
